591b3d56f79bab2374a6478c420ceadc7b89b577
julienlocatelli <julien.locatelli@gmail.com>
2017-07-05 16:36:21 +0200

    format millisecond

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,19 @@
 import moment from 'moment';
 
+function formatMilliSecond(millisecond) {
+
+    console.log(millisecond);
+
+    if(millisecond < 10)
+        return `00${millisecond}`;
+
+    if(millisecond < 100)
+        return `0${millisecond}`;
+
+    return millisecond;
+}
+
 export function renderFormattedTime(milliseconds) {
     const duration = moment.duration({milliseconds});
-    return `${duration.seconds()}' ${duration.milliseconds()}''`
+    return `${duration.seconds()}'${formatMilliSecond(duration.milliseconds())}''`
 }
